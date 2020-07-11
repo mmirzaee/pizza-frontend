45ef607591d252f58c8adfaa6476e4184df30eb7
Ali M.Mirzaee <me@alimrz.com>
2020-07-11 22:02:30 +0430

    Added react-router-dom to the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Home from './components/pages/home/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Route exact path="/" component={Home}/>
+        </Router>
+    )
 }
 
-export default App;
+export default App
