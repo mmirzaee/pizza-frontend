b050e4c60ac27e652218fd2fe1b8623ea85e2ed3
Ali M.Mirzaee <me@alimrz.com>
2020-07-19 21:21:00 +0430

    Clean up orders page

--- src/components/pages/history/OrderHistory.js
+++ src/components/pages/history/OrderHistory.js
@@ -1,11 +1,9 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect} from 'react';
 import Menu from "../../general/Menu";
 import {connect} from "react-redux";
-import {compose} from "redux";
 import Api from "../../../api/Api";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
-import {withRouter} from 'react-router-dom'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -23,44 +21,41 @@ const mapStateToProps = (state) => ({
     profile: state.profile,
 });
 
-class History extends Component {
+function OrderHistory(props) {
+    const [loading, setLoading] = React.useState(true);
+    const [orderHistoryItems, setOrderHistoryItems] = React.useState(null);
 
-    constructor(props) {
-        super(props);
-        this.state = {isLoading: true, orderHistoryItems: null}
-    }
-
-
-    componentDidMount() {
-        const {token} = this.props;
+    useEffect(() => {
+        const {token} = props;
         Api.getOrdersHistory(token).then((res) => {
-            this.setState({orderHistoryItems: res, isLoading: false})
+            setLoading(false);
+            setOrderHistoryItems(res);
         })
-    }
+    }, []);
 
-    render() {
-        const {isLoading, orderHistoryItems} = this.state;
-        const {token} = this.props;
-        const statusEnum = {'1': 'Successfully Submitted', '2': 'In The Way', '3': 'Delivered', '0': 'Cancelled'};
-        return <>
-            <Menu showCart={true}/>
-            <Container fixed>
-                <Grid container item xs={12}
+    const {token} = props;
+    const statusEnum = {'1': 'Successfully Submitted', '2': 'In The Way', '3': 'Delivered', '0': 'Cancelled'};
+
+    return <>
+        <Menu showCart={true}/>
+        <Container fixed>
+            <Grid container item xs={12}
+                  direction="row"
+                  justify="center"
+            >
+                <Grid container item xs={12} spacing={2}
                       direction="row"
                       justify="center"
                 >
-                    <Grid container item xs={12} spacing={2}
-                          direction="row"
-                          justify="center"
-                    >
-                        <h2 className="font-300">Your orders history</h2>
-                    </Grid>
-                    <Grid xs={12} container item spacing={5}
-                    >
-                        {isLoading &&
-                        <CircularProgress color="inherit"/>
-                        }
-                        {!isLoading &&
+                    <h2 className="font-300">Your orders history</h2>
+                </Grid>
+                <Grid xs={12}
+                      container
+                      item
+                      spacing={5}
+                      justify="center"
+                >
+                    {loading ? <CircularProgress color="inherit"/> :
                         <TableContainer component={Paper}>
                             <Table>
                                 <TableHead>
@@ -72,7 +67,7 @@ class History extends Component {
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {orderHistoryItems && orderHistoryItems.map((row) => (
+                                    {token && orderHistoryItems && orderHistoryItems.map((row) => (
                                         <TableRow key={row}>
                                             <TableCell component="th" scope="row">
                                                 {row.created_at}
@@ -93,20 +88,16 @@ class History extends Component {
                                 </TableBody>
                             </Table>
                         </TableContainer>
-                        }
-
-                    </Grid>
+                    }
 
                 </Grid>
-                {!token &&
-                <Alert fullWidth severity="info">Only logged in users can see their order history</Alert>
-                }
-            </Container>
-        </>
-    }
+
+            </Grid>
+            {!token &&
+            <Alert fullWidth severity="info">Only logged in users can see their order history</Alert>
+            }
+        </Container>
+    </>
 }
 
-export default compose(
-    withRouter,
-    connect(mapStateToProps)
-)(History);
+export default connect(mapStateToProps)(OrderHistory)
