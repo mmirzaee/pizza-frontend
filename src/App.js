8e62b206a023293db0ab6c160ca87107efb1bc90
Ali M.Mirzaee <me@alimrz.com>
2020-07-19 21:19:55 +0430

    Clean up app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Home from './components/pages/home/Home';
 import Checkout from './components/pages/checkout/Checkout';
-import History from './components/pages/history/History';
+import OrderHistory from './components/pages/history/OrderHistory';
 import {Provider} from 'react-redux';
 import UpdateCart from './reducers/UpdateCart';
 import {createStore} from 'redux';
@@ -10,11 +10,11 @@ import storage from 'redux-persist/lib/storage';
 import {persistReducer, persistStore} from 'redux-persist';
 import {PersistGate} from 'redux-persist/integration/react'
 
-
 const persistConfig = {
     key: 'root',
     storage,
 };
+
 const persistedReducer = persistReducer(persistConfig, UpdateCart)
 
 const store = createStore(
@@ -31,7 +31,7 @@ function App() {
                     <Router>
                         <Route exact path="/" component={Home}/>
                         <Route exact path="/checkout" component={Checkout}/>
-                        <Route exact path="/history" component={History}/>
+                        <Route exact path="/orders" component={OrderHistory}/>
                     </Router>
                 </div>
             </PersistGate>
